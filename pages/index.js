2541307488c3ca4bdf7e5929182c39d0ac9210db
chandanhm1999 <109410990+chandanhm1999@users.noreply.github.com>
2023-03-14 19:00:23 +0530

    reason for editing

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Pizza Restaurant!</title>
         <meta name="description" content="Best pizza shop in town" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/img/checked.png" />
       </Head>
       <Featured />
       <PizzaList />
